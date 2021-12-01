4f600421947026b216890cf08eeece9223e605c3
Andreas Houben <andreas.houben.ext@devk.de>
2021-12-01 16:11:48 +0100

    fix footer

--- app/root.tsx
+++ app/root.tsx
@@ -9,11 +9,17 @@ export default function App() {
     <html lang="de">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport"  content= "width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Anzeige der Coronazahlen von Gladbeck, 
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="Anzeige der Coronazahlen von Gladbeck, 
         bereitgestellt vom Kreis Recklinghausen. Die Seite zeigt stellt die Zahlen 
-        textuell dar und gibt einen Überblick über die täglichen Veränderungen."/>        
-        <meta name="theme-color" content="#000000"/>
+        textuell dar und gibt einen Überblick über die täglichen Veränderungen."
+        />
+        <meta name="theme-color" content="#000000" />
         <Meta />
         <Links />
       </head>
@@ -21,11 +27,19 @@ export default function App() {
         <Outlet />
 
         {includeScripts && <Scripts />}
+        <footer>
+          <div>
+            Favicon by{" "}
+            <a href="https://www.flaticon.com/de/autoren/surang" title="surang">
+              surang
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/de/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </div>
+        </footer>
       </body>
-      <footer>
-    <div>Favicon by <a href="https://www.flaticon.com/de/autoren/surang" title="surang">surang</a> from <a
-            href="https://www.flaticon.com/de/" title="Flaticon">www.flaticon.com</a></div>
-</footer>
     </html>
   );
 }
